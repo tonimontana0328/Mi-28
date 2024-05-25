eeaa250f123bac0b9de7910cb9127b516ed0ec3d
SpaceStrider <80745119+SpaceStrider@users.noreply.github.com>
2024-05-25 12:25:00 +1000

    Initial commit

---
+++ scripts/Game/generated/Weapon/MI28MagazineWell.c
@@ -0,0 +1,7 @@
+//Mark 7 - 16 Inch
+class MagazineWell2A42MI28Class {}
+MagazineWell2A42MI28Class MagazineWell2A42MI28Source;
+
+class MagazineWell2A42MI28 : BaseMagazineWell
+{
+};
---
+++ scripts/Game/generated/Weapon/SPC_RocketPodsMagazineWellRU.c
@@ -0,0 +1,23 @@
+//S-8
+class MagazineWellB8V20AClass {}
+MagazineWellB8V20AClass MagazineWellB8V20ASource;
+
+class MagazineWellB8V20A : BaseMagazineWell
+{
+};
+
+//Ataka
+class MagazineWell9M120AtakaClass {}
+MagazineWell9M120AtakaClass MagazineWell9M120AtakaSource;
+
+class MagazineWell9M120Ataka : BaseMagazineWell
+{
+};
+
+//BGM71 TOW
+class MagazineWell9K38IglaClass {}
+MagazineWell9K38IglaClass MagazineWell9K38IglaSource;
+
+class MagazineWell9K38Igla : BaseMagazineWell
+{
+};
